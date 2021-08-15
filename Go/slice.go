91cdfea94f0e41b722861806c6e2b5fcc0e54788
Cheejyg <glenncheejunyuan@hotmail.com>
2021-08-16 02:13:50 +0800

    Update slice.go

--- Go/slice.go
+++ Go/slice.go
@@ -47,7 +47,13 @@ func StringsToPtrSlicePtrString(strings_ ...string) (ptr_slice_ptr_string_ *[]*s
 
 // StringsToPtrSliceString parses string(s) strings_ to *[]string.
 func StringsToPtrSliceString(strings_ ...string) (ptr_slice_string_ *[]string) {
-	return &strings_
+	slice_string_ := make([]string, len(strings_))
+
+	copy(slice_string_, strings_)
+
+	ptr_slice_string_ = &slice_string_
+
+	return
 }
 
 // Uint64sToSlicePtrUint64 parses uint64(s) uint64s_ to []*uint64.
@@ -89,7 +95,13 @@ func Uint64sToPtrSlicePtrUint64(uint64s_ ...uint64) (ptr_slice_ptr_uint64_ *[]*u
 
 // Uint64sToPtrSliceUint64 parses uint64(s) uint64s_ to *[]uint64.
 func Uint64sToPtrSliceUint64(uint64s_ ...uint64) (ptr_slice_uint64_ *[]uint64) {
-	return &uint64s_
+	slice_uint64_ := make([]uint64, len(uint64s_))
+
+	copy(slice_uint64_, uint64s_)
+
+	ptr_slice_uint64_ = &slice_uint64_
+
+	return
 }
 
 // Uint32sToSlicePtrUint32 parses uint32(s) uint32s_ to []*uint32.
@@ -131,7 +143,13 @@ func Uint32sToPtrSlicePtrUint32(uint32s_ ...uint32) (ptr_slice_ptr_uint32_ *[]*u
 
 // Uint32sToPtrSliceUint32 parses uint32(s) uint32s_ to *[]uint32.
 func Uint32sToPtrSliceUint32(uint32s_ ...uint32) (ptr_slice_uint32_ *[]uint32) {
-	return &uint32s_
+	slice_uint32_ := make([]uint32, len(uint32s_))
+
+	copy(slice_uint32_, uint32s_)
+
+	ptr_slice_uint32_ = &slice_uint32_
+
+	return
 }
 
 // Uint16sToSlicePtrUint16 parses uint16(s) uint16s_ to []*uint16.
@@ -173,7 +191,13 @@ func Uint16sToPtrSlicePtrUint16(uint16s_ ...uint16) (ptr_slice_ptr_uint16_ *[]*u
 
 // Uint16sToPtrSliceUint16 parses uint16(s) uint16s_ to *[]uint16.
 func Uint16sToPtrSliceUint16(uint16s_ ...uint16) (ptr_slice_uint16_ *[]uint16) {
-	return &uint16s_
+	slice_uint16_ := make([]uint16, len(uint16s_))
+
+	copy(slice_uint16_, uint16s_)
+
+	ptr_slice_uint16_ = &slice_uint16_
+
+	return
 }
 
 // Uint8sToSlicePtrUint8 parses uint8(s) uint8s_ to []*uint8.
@@ -215,7 +239,13 @@ func Uint8sToPtrSlicePtrUint8(uint8s_ ...uint8) (ptr_slice_ptr_uint8_ *[]*uint8)
 
 // Uint8sToPtrSliceUint8 parses uint8(s) uint8s_ to *[]uint8.
 func Uint8sToPtrSliceUint8(uint8s_ ...uint8) (ptr_slice_uint8_ *[]uint8) {
-	return &uint8s_
+	slice_uint8_ := make([]uint8, len(uint8s_))
+
+	copy(slice_uint8_, uint8s_)
+
+	ptr_slice_uint8_ = &slice_uint8_
+
+	return
 }
 
 // UintsToSlicePtrUint parses uint(s) uints_ to []*uint.
@@ -257,7 +287,13 @@ func UintsToPtrSlicePtrUint(uints_ ...uint) (ptr_slice_ptr_uint_ *[]*uint) {
 
 // UintsToPtrSliceUint parses uint(s) uints_ to *[]uint.
 func UintsToPtrSliceUint(uints_ ...uint) (ptr_slice_uint_ *[]uint) {
-	return &uints_
+	slice_uint_ := make([]uint, len(uints_))
+
+	copy(slice_uint_, uints_)
+
+	ptr_slice_uint_ = &slice_uint_
+
+	return
 }
 
 // Int64sToSlicePtrInt64 parses int64(s) int64s_ to []*int64.
@@ -299,7 +335,13 @@ func Int64sToPtrSlicePtrInt64(int64s_ ...int64) (ptr_slice_ptr_int64_ *[]*int64)
 
 // Int64sToPtrSliceInt64 parses int64(s) int64s_ to *[]int64.
 func Int64sToPtrSliceInt64(int64s_ ...int64) (ptr_slice_int64_ *[]int64) {
-	return &int64s_
+	slice_int64_ := make([]int64, len(int64s_))
+
+	copy(slice_int64_, int64s_)
+
+	ptr_slice_int64_ = &slice_int64_
+
+	return
 }
 
 // Int32sToSlicePtrInt32 parses int32(s) int32s_ to []*int32.
@@ -341,7 +383,13 @@ func Int32sToPtrSlicePtrInt32(int32s_ ...int32) (ptr_slice_ptr_int32_ *[]*int32)
 
 // Int32sToPtrSliceInt32 parses int32(s) int32s_ to *[]int32.
 func Int32sToPtrSliceInt32(int32s_ ...int32) (ptr_slice_int32_ *[]int32) {
-	return &int32s_
+	slice_int32_ := make([]int32, len(int32s_))
+
+	copy(slice_int32_, int32s_)
+
+	ptr_slice_int32_ = &slice_int32_
+
+	return
 }
 
 // Int16sToSlicePtrInt16 parses int16(s) int16s_ to []*int16.
@@ -383,7 +431,13 @@ func Int16sToPtrSlicePtrInt16(int16s_ ...int16) (ptr_slice_ptr_int16_ *[]*int16)
 
 // Int16sToPtrSliceInt16 parses int16(s) int16s_ to *[]int16.
 func Int16sToPtrSliceInt16(int16s_ ...int16) (ptr_slice_int16_ *[]int16) {
-	return &int16s_
+	slice_int16_ := make([]int16, len(int16s_))
+
+	copy(slice_int16_, int16s_)
+
+	ptr_slice_int16_ = &slice_int16_
+
+	return
 }
 
 // Int8sToSlicePtrInt8 parses int8(s) int8s_ to []*int8.
@@ -425,7 +479,13 @@ func Int8sToPtrSlicePtrInt8(int8s_ ...int8) (ptr_slice_ptr_int8_ *[]*int8) {
 
 // Int8sToPtrSliceInt8 parses int8(s) int8s_ to *[]int8.
 func Int8sToPtrSliceInt8(int8s_ ...int8) (ptr_slice_int8_ *[]int8) {
-	return &int8s_
+	slice_int8_ := make([]int8, len(int8s_))
+
+	copy(slice_int8_, int8s_)
+
+	ptr_slice_int8_ = &slice_int8_
+
+	return
 }
 
 // IntsToSlicePtrInt parses int(s) ints_ to []*int.
@@ -467,7 +527,13 @@ func IntsToPtrSlicePtrInt(ints_ ...int) (ptr_slice_ptr_int_ *[]*int) {
 
 // IntsToPtrSliceInt parses int(s) ints_ to *[]int.
 func IntsToPtrSliceInt(ints_ ...int) (ptr_slice_int_ *[]int) {
-	return &ints_
+	slice_int_ := make([]int, len(ints_))
+
+	copy(slice_int_, ints_)
+
+	ptr_slice_int_ = &slice_int_
+
+	return
 }
 
 // BoolsToSlicePtrBool parses bool(s) bools_ to []*bool.
@@ -509,5 +575,11 @@ func BoolsToPtrSlicePtrBool(bools_ ...bool) (ptr_slice_ptr_bool_ *[]*bool) {
 
 // BoolsToPtrSliceBool parses bool(s) bools_ to *[]bool.
 func BoolsToPtrSliceBool(bools_ ...bool) (ptr_slice_bool_ *[]bool) {
-	return &bools_
+	slice_bool_ := make([]bool, len(bools_))
+
+	copy(slice_bool_, bools_)
+
+	ptr_slice_bool_ = &slice_bool_
+
+	return
 }
