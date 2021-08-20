b5c5c703c5a2c539ceb25deff19211b1f14d077a
Cheejyg <glenncheejunyuan@hotmail.com>
2021-08-20 22:26:13 +0800

    Update Conv.java

--- Java/src/com/github/cheejyg/conv/java/Conv.java
+++ Java/src/com/github/cheejyg/conv/java/Conv.java
@@ -40,8 +40,217 @@ public class Conv {
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Int conversions
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * IntToString parses <code>int</code> int_ to <code>String</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param int_ The <code>int</code> to be parsed.
+	 * @return <code>String</code>
+	 */
+	public static String IntToString(int int_) {
+		String string_ = ToString(int_);
+
+		return string_;
+	}
+
+	/**
+	 * IntToLong parses <code>int</code> int_ to <code>Long</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param int_ The <code>int</code> to be parsed.
+	 * @return <code>Long</code>
+	 */
+	public static Long IntToLong(int int_) {
+		Long long_ = new Long(int_);
+
+		return long_;
+	}
+
+	/**
+	 * IntToInteger parses <code>int</code> int_ to <code>Integer</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param int_ The <code>int</code> to be parsed.
+	 * @return <code>Integer</code>
+	 */
+	public static Integer IntToInteger(int int_) {
+		return int_;
+	}
+
+	/**
+	 * IntToShort parses <code>int</code> int_ to <code>Short</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param int_ The <code>int</code> to be parsed.
+	 * @return <code>Short</code>
+	 */
+	public static Short IntToShort(int int_) {
+		Short short_ = new Short((short) int_);
+
+		return short_;
+	}
+
+	/**
+	 * IntToByte parses <code>int</code> int_ to <code>Byte</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param int_ The <code>int</code> to be parsed.
+	 * @return <code>Byte</code>
+	 */
+	public static Byte IntToByte(int int_) {
+		Byte byte_ = new Byte((byte) int_);
+
+		return byte_;
+	}
+
+	/**
+	 * IntToBoolean parses <code>int</code> int_ to <code>Boolean</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param int_ The <code>int</code> to be parsed.
+	 * @return <code>Boolean</code>
+	 */
+	public static Boolean IntToBoolean(int int_) {
+		if (int_ == 0) {
+			return false;
+		} else if (int_ == 1) {
+			return true;
+		} else {
+		}
+
+		return null;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Short conversions
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * ShortToString parses <code>short</code> short_ to <code>String</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param short_ The <code>short</code> to be parsed.
+	 * @return <code>String</code>
+	 */
+	public static String ShortToString(short short_) {
+		String string_ = ToString(short_);
+
+		return string_;
+	}
+
+	/**
+	 * ShortToLong parses <code>short</code> short_ to <code>Long</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param short_ The <code>short</code> to be parsed.
+	 * @return <code>Long</code>
+	 */
+	public static Long ShortToLong(short short_) {
+		Long long_ = new Long(short_);
+
+		return long_;
+	}
+
+	/**
+	 * ShortToInteger parses <code>short</code> short_ to <code>Integer</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param short_ The <code>short</code> to be parsed.
+	 * @return <code>Integer</code>
+	 */
+	public static Integer ShortToInteger(short short_) {
+		Integer integer_ = new Integer(short_);
+
+		return integer_;
+	}
+
+	/**
+	 * ShortToShort parses <code>short</code> short_ to <code>Short</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param short_ The <code>short</code> to be parsed.
+	 * @return <code>Short</code>
+	 */
+	public static Short ShortToShort(short short_) {
+		return short_;
+	}
+
+	/**
+	 * ShortToByte parses <code>short</code> short_ to <code>Byte</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param short_ The <code>short</code> to be parsed.
+	 * @return <code>Byte</code>
+	 */
+	public static Byte ShortToByte(short short_) {
+		Byte byte_ = new Byte((byte) short_);
+
+		return byte_;
+	}
+
+	/**
+	 * ShortToBoolean parses <code>short</code> short_ to <code>Boolean</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param short_ The <code>short</code> to be parsed.
+	 * @return <code>Boolean</code>
+	 */
+	public static Boolean ShortToBoolean(short short_) {
+		if (short_ == 0) {
+			return false;
+		} else if (short_ == 1) {
+			return true;
+		} else {
+		}
+
+		return null;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Byte conversions
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * ByteToString parses <code>byte</code> byte_ to <code>String</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param byte_ The <code>byte</code> to be parsed.
+	 * @return <code>String</code>
+	 */
+	public static String ByteToString(byte byte_) {
+		String string_ = ToString(byte_);
+
+		return string_;
+	}
+
+	/**
+	 * ByteToLong parses <code>byte</code> byte_ to <code>Long</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param byte_ The <code>byte</code> to be parsed.
+	 * @return <code>Long</code>
+	 */
+	public static Long ByteToLong(byte byte_) {
+		Long long_ = new Long(byte_);
+
+		return long_;
+	}
+
+	/**
+	 * ByteToInteger parses <code>byte</code> byte_ to <code>Integer</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param byte_ The <code>byte</code> to be parsed.
+	 * @return <code>Integer</code>
+	 */
+	public static Integer ByteToInteger(byte byte_) {
+		Integer integer_ = new Integer(byte_);
+
+		return integer_;
+	}
 
 	/**
 	 * ByteToShort parses <code>byte</code> byte_ to <code>Short</code>.
