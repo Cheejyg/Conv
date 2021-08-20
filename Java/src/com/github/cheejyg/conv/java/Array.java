67951468ee6d4137d673cf59133f0ac1da0101a7
Cheejyg <glenncheejunyuan@hotmail.com>
2021-08-21 00:03:36 +0800

    Update Array.java

--- Java/src/com/github/cheejyg/conv/java/Array.java
+++ Java/src/com/github/cheejyg/conv/java/Array.java
@@ -15,6 +15,52 @@ public class Array {
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	/**
+	 * LongsToLongArray parses <code>Long[]</code> longs_ to <code>Long[]</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param longs_ The <code>Long[]</code> to be parsed.
+	 * @return <code>Long[]</code>
+	 */
+	public static java.lang.Long[] LongsToLongArray(java.lang.Long... longs_) {
+		return longs_;
+	}
+
+	/**
+	 * IntegersToIntegerArray parses <code>Integer[]</code> integers_ to
+	 * <code>Integer[]</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param integers_ The <code>Integer[]</code> to be parsed.
+	 * @return <code>Integer[]</code>
+	 */
+	public static java.lang.Integer[] IntegersToIntegerArray(java.lang.Integer... integers_) {
+		return integers_;
+	}
+
+	/**
+	 * ShortsToShortArray parses <code>Short[]</code> shorts_ to
+	 * <code>Short[]</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param shorts_ The <code>Short[]</code> to be parsed.
+	 * @return <code>Short[]</code>
+	 */
+	public static java.lang.Short[] ShortsToShortArray(java.lang.Short... shorts_) {
+		return shorts_;
+	}
+
+	/**
+	 * BytesToByteArray parses <code>Byte[]</code> bytes_ to <code>Byte[]</code>.
+	 * 
+	 * @author Cheejyg
+	 * @param bytes_ The <code>Byte[]</code> to be parsed.
+	 * @return <code>Byte[]</code>
+	 */
+	public static java.lang.Byte[] BytesToByteArray(java.lang.Byte... bytes_) {
+		return bytes_;
+	}
+
+	/**
 	 * BooleansToBooleanArray parses <code>Boolean[]</code> booleans_ to
 	 * <code>Boolean[]</code>.
 	 * 
